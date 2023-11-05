8ca0a9cca8584cce1a089f542cd4947722d83cb2
jos-gabriel <jos.gabrie@gmail.com>
2023-11-05 15:09:55 -0600

    ft(exeption)

---
+++ src/main/java/com/softtek/determinacion/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.softtek.determinacion.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequestException(BadRequestException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
