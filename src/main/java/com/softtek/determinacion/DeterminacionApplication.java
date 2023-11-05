d1052b4167181e785c32a06b769d92db7a36a63a
jos-gabriel <jos.gabrie@gmail.com>
2023-11-04 19:48:48 -0600

    refactor(firstCommit)

--- src/main/java/com/softtek/determinacion/DeterminacionApplication.java
+++ src/main/java/com/softtek/determinacion/DeterminacionApplication.java
@@ -1,4 +1,4 @@
-package com.softtek.Determinacion;
+package com.softtek.determinacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/softtek/determinacion/DeterminacionApplicationTests.java
+++ src/test/java/com/softtek/determinacion/DeterminacionApplicationTests.java
@@ -1,4 +1,4 @@
-package com.softtek.Determinacion;
+package com.softtek.determinacion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
