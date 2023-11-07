854700e1ddd7446ab70a1ed60b5f8bb870fdfd25
jos-gabriel <jos.gabrie@gmail.com>
2023-11-07 09:42:59 -0600

    ft(modelsLey73y97)Input Output

---
+++ src/main/java/com/softtek/determinacion/model/response/ley73/DeterminacionLey73Model.java
@@ -0,0 +1,13 @@
+package com.softtek.determinacion.model.response.ley73;
+
+import lombok.Data;
+
+@Data
+public class DeterminacionLey73Model {
+    private String derecho;
+    private String mensaje;
+    private String claveFundamento;
+    private String fundamento;
+    private String claveObservacion;
+    private String observaciones;
+}
---
+++ src/main/java/com/softtek/determinacion/model/response/ley97/DeterminacionLey97Model.java
@@ -0,0 +1,19 @@
+package com.softtek.determinacion.model.response.ley97;
+
+import lombok.Data;
+
+@Data
+public class DeterminacionLey97Model {
+    private String derecho;
+    private String mensaje;
+    private String claveFundamento;
+    private String fundamento;
+    private String claveObservacion;
+    private String observaciones;
+    private String derechoSM;
+    private String mensajeSM;
+    private String claveFundamentoSM;
+    private String fundamentoSM;
+    private String claveObservacionsSM;
+    private String observacionesSM;
+}
